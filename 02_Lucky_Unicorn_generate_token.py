2f30759e6101ed51d7118a3d78ca0bbfd1d2f602
rustt71749 <60583446+rustt71749@users.noreply.github.com>
2020-02-21 10:38:24 +1300

    Component 2

---
+++ 02_Lucky_Unicorn_generate_token.py
@@ -0,0 +1,14 @@
+# Lucky Unicorn Decomposition Step 2
+# Generate a random token
+
+import random
+
+tokens = ["unicorn", "donkey", "horse", "zebra"]
+
+for item in range(1,15):
+
+    chosen = random.choice(tokens)
+    print(chosen)
+
+
+
