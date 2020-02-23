be637efc6e5e8da69d5e547df020be3200ca4dfa
rustt71749 <60583446+rustt71749@users.noreply.github.com>
2020-02-24 09:38:41 +1300

    03_Lucky_Unicorn_payout_calculator_V2

--- 03_Lucky_Unicorn_payout_calculator.py
+++ 03_Lucky_Unicorn_payout_calculator.py
@@ -11,5 +11,27 @@
 
 import random
 
-how_much = 100
+HOW_MUCH = 100
 tokens = ["horse", "zebra", "donkey", "unicorn"]
+
+unicorn_count = 0
+zebhor_count = 0
+donkey_count = 0
+
+for item in range(1,HOW_MUCH):
+
+    chosen = random.choice(tokens)
+
+    if chosen == "unicorn":
+        unicorn_count += 1
+    elif chosen == "donkey":
+        donkey_count += 1
+    else:
+        zebhor_count += 1
+
+    print(chosen)
+
+print("Win / Loss Calculations")
+print("# Unicorns: {}".format(unicorn_count))
+print("# Zebra / Horses: {}".format(zebhor_count))
+print("# Donkeys: {}".format(donkey_count))
